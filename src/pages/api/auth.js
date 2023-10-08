06c5a2016f6148a25ba27e00a144ffc954c784a1
awais-codes <awais.codes1@gmail.com>
2023-10-09 02:44:18 +0500

    feat: added sse

--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -1,5 +1,25 @@
+let clients = [];
+
 const requestHandler = (req, res) => {
-    res.status(200).json({ message: 'HALOOO' });
+    // TODO: Add logic to add to clients if its not already added.
+    clients.push(res);
+
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+    for (const client of clients) {
+        // Send an event to the client
+        const randomValue = Math.random().toFixed(2);
+        client.write(`data: Random Value: ${randomValue}\n\n`);
+    }
+
+    // Close the connection when the client disconnects
+    req.on('close', () => {
+        // Remove res from clients list
+        clients = clients.filter((client) => client !== res);
+        res.end();
+    });
 };
 
 export default requestHandler;
