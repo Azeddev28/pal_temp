9544842d8e5a367c36432a944271e0b9dad57be6
Abdul Hanan Asif <99734167+abdulhananasif@users.noreply.github.com>
2023-10-14 02:48:58 +0500

    chore: removed unused code

--- src/pages/api/updateMessage.js
+++ src/pages/api/updateMessage.js
@@ -1,12 +1,8 @@
-import { setEventMessage } from '@/store/authSlice';
-import { makeStore } from '@/store/store';
 import { client } from '@/utils';
 
 export default (req, res) => {
     if (req.method === 'POST') {
-        const store = makeStore();
-        if (client && store) {
-            store.dispatch(setEventMessage({ ...req.body }));
+        if (client) {
             client.write(
                 `data: Message: ${JSON.stringify({ ...req.body })}\n\n`
             );
