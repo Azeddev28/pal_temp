7942a6923a9ac214426c1b64d4ab83c32758e732
Abdul Hanan Asif <abdulhananasif5643@gmail.com>
2023-10-16 19:54:36 +0500

    chore: variable changed

--- src/views/SignUp/ProfileFormModal/Steps/index.jsx
+++ src/views/SignUp/ProfileFormModal/Steps/index.jsx
@@ -3,6 +3,7 @@ import { Input, Radio } from '@/components/Input';
 import { Typography } from '@/components/Typography';
 import { useMemo, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
+import { useSelector } from 'react-redux';
 import { useStep } from './context';
 
 const Step1 = () => {
@@ -144,7 +145,7 @@ const Step2 = () => {
 
 const Step3 = () => {
     const { register, watch, formState } = useFormContext();
-
+    const { firstName, lastName } = useSelector((state) => state.auth);
     return (
         <div className="pt-16">
             <div className="mb-12">
