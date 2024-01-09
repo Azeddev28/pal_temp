b9602aa04b474f394953f952915cc00c8ec25ae9
Saad  Akbar <saadakbar8@gmail.com>
2024-01-09 22:47:03 +0500

    chore: update server urls

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,15 @@
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 const ROUTES = {
-    companiesList: `${SERVER_URL}/api/companies/companies-list/`,
-    industriesList: `${SERVER_URL}/api/companies/industries-list/`,
-    countriesList: `${SERVER_URL}/api/locations/countries-list/`,
-    languagesList: `${SERVER_URL}/api/locations/languages-list/`,
-    joinWaitlist: `/api/users/join-waitlist/`,
-    profileRegister: '/api/users/profile-register/',
-    googleLink: `${SERVER_URL}/api/authentication/google-register`,
-    linkedInLink: `${SERVER_URL}/api/authentication/linkedin-register`,
-    githubLink: `${SERVER_URL}/api/authentication/github-register`,
+    companiesList: `${SERVER_URL}/d/api/companies/companies-list/`,
+    industriesList: `${SERVER_URL}/d/api/companies/industries-list/`,
+    countriesList: `${SERVER_URL}/d/api/locations/countries-list/`,
+    languagesList: `${SERVER_URL}/d/api/locations/languages-list/`,
+    joinWaitlist: `/d/api/users/join-waitlist/`,
+    profileRegister: '/d/api/users/profile-register/',
+    googleLink: `${SERVER_URL}/d/api/authentication/google-register`,
+    linkedInLink: `${SERVER_URL}/d/api/authentication/linkedin-register`,
+    githubLink: `${SERVER_URL}/d/api/authentication/github-register`,
 };
 
 export const getRoute = (routeKey) => ROUTES[routeKey];
