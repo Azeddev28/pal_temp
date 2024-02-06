6ee10afd2e5d6a4393a40af8ff351947889aec40
Abdul Hanan Asif <99734167+abdulhananasif@users.noreply.github.com>
2024-02-06 19:07:39 +0500

    padding added

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,8 +1,7 @@
-import { withAuthorization } from '@/auth/withAuthorization';
 import { SignUp } from '@/views/SignUp';
 
 const Page = () => {
     return <SignUp />;
 };
 
-export default withAuthorization(Page);
+export default Page;
