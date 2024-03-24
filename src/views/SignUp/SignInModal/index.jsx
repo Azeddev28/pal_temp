d345b68ffa40b4351346155e35350bf6f82ac294
Fizzakhan094 <Fizzakhan9094@gmail.com>
2024-03-25 01:56:57 +0500

    Images updation in 2 pages 1. SignupPage 2.SignInModal

--- src/views/SignUp/SignInModal/index.jsx
+++ src/views/SignUp/SignInModal/index.jsx
@@ -1,5 +1,5 @@
 import { Modal } from '@/components/Modal';
-import illustrationBoxImage from '../../../../public/images/illustration-box.webp';
+import SignInPagePicture from '../../../../public/images/SignInPagePicture.svg';
 import { GithubButton, GoogleButton, LinkedInButton } from './Buttons';
 
 const SignInModal = ({ isOpen, onClose }) => {
@@ -7,7 +7,7 @@ const SignInModal = ({ isOpen, onClose }) => {
         <Modal
             isOpen={isOpen}
             onClose={onClose}
-            bannerImageSrc={illustrationBoxImage}
+            bannerImageSrc={SignInPagePicture}
         >
             <div className="flex justify-center pt-9 pb-7">
                 <div className="flex flex-col gap-4 w-full px-10">
--- src/views/SignUp/index.jsx
+++ src/views/SignUp/index.jsx
@@ -1,9 +1,11 @@
 import { Button } from '@/components/Button';
 import { Typography } from '@/components/Typography';
+import Image from 'next/image';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import AnnouncementRobo from '../../../public/images/AnnouncementRobo.svg';
 import { ProfileFormModal } from './ProfileFormModal';
-
+import { SignInModal } from './SignInModal';
 const SignUp = () => {
     const { isUserRegistered, hasJoinedWaitList } = useSelector((state) => {
         return state.auth;
@@ -28,34 +30,27 @@ const SignUp = () => {
         }
     };
     return (
-        <div className="flex items-center justify-center h-screen">
-            <div className="flex flex-row w-auto mb-14">
-                <div className="max-w-[42rem] body-font flex flex-col font-poppins md:p-10">
-                    <div className="mb-8 text-center">
-                        <Typography
-                            variant={'h2'}
-                            className="mb-4 text-brandBlue dark:text-brandBlue"
-                        >
-                            Your spot is reserved! 🎉
-                        </Typography>
-                    </div>
-                    <div className="mb-10 text-center font-semibold">
-                        <Typography variant={'h5'} className="text-grey20 ">
-                            You're on the list! We'll let you know when your
-                            spot to sign up is ready
-                        </Typography>
-                    </div>
-                    <div className="mb-6 text-center font-semibold">
-                        <Typography variant={'h5'} className="text-grey40">
-                            If you'd like to move up in line, sign up and
-                            complete your profile set up now
-                        </Typography>
-                    </div>
-                    <div className="flex gap-4 justify-center">
-                        <Button size="sm" onClick={() => setIsModalOpen(true)}>
-                            <Typography variant={'body'}>Sign up</Typography>
-                        </Button>
-                    </div>
+        <div className="flex justify-center items-center flex-col h-screen font-poppins">
+            <div className="flex flex-col gap-[3.125rem] max-w-[44.94rem] ">
+                <div className="flex flex-col items-center gap-[2.5rem]">
+                    <Image src={AnnouncementRobo} />
+                    <Typography variant={'h2'} className="text-brandBlue">
+                        Your spot is reserved! 🎉
+                    </Typography>
+
+                    <Typography variant={'h5'} className="text-grey20 ">
+                        You're on the list! We'll let you know when your spot to
+                        sign up is ready
+                    </Typography>
+                </div>
+                <div className="flex flex-col items-center gap-4">
+                    <Typography variant={'h5'} className="text-grey40">
+                        If you'd like to move up in line, sign up and complete
+                        your profile set up now
+                    </Typography>
+                    <Button size="sm" onClick={() => setIsModalOpen(true)}>
+                        <Typography variant={'body'}>Sign up</Typography>
+                    </Button>
                 </div>
             </div>
             {renderModal()}
