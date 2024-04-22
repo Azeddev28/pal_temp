a9f6b5a890183f5505f1ea3433780f81891ea146
Fizzakhan094 <Fizzakhan9094@gmail.com>
2024-04-22 19:40:39 +0500

    Tooltip Fix

--- src/components/Tooltip/index.jsx
+++ src/components/Tooltip/index.jsx
@@ -1,8 +1,10 @@
 const Tooltip = ({ text, tooltip }) => {
     return (
         <div className="has-tooltip">
-            <p className="text-sm font-normal text-grey20 underline">{text}</p>
-            <p className="tooltip text-xs rounded-lg shadow-lg p-2 bg-gray-100 text-grey20 border-gray-800 -mt-24 -ml-32 w-64 h-16">
+            <p className="cursor-pointer pl-[10px] text-sm font-normal text-grey20 underline">
+                {text}
+            </p>
+            <p className=" tooltip w-full left-[125px] text-[9px] screen_360:text-xs rounded-lg shadow-lg p-2 bg-gray-100 text-grey20 border-gray-800 -mt-24 -ml-32  h-16">
                 {tooltip}
             </p>
         </div>
