fba09de22be80d46ca250dd277a3eb0493659a30
Fizzakhan094 <Fizzakhan9094@gmail.com>
2024-04-22 20:09:24 +0500

    sign up page fix

--- src/views/SignUp/index.jsx
+++ src/views/SignUp/index.jsx
@@ -1,11 +1,9 @@
-import { Button } from '@/components/Button';
 import Image from 'next/image';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import AnnouncementRobo from '../../../public/images/AnnouncementRobo.svg';
 import { ProfileFormModal } from './ProfileFormModal';
 import { SignInModal } from './SignInModal';
-
 const SignUp = () => {
     const { isUserRegistered, hasJoinedWaitList } = useSelector((state) => {
         return state.auth;
@@ -48,9 +46,13 @@ const SignUp = () => {
                         If you'd like to move up in line, sign up and complete
                         your profile set up now
                     </p>
-                    <Button size="sm" onClick={() => setIsModalOpen(true)}>
+                    <button
+                        className="w-[156px] bg-brandBlue h-12 rounded-lg"
+                        size="sm"
+                        onClick={() => setIsModalOpen(true)}
+                    >
                         <p className="subHeading5">Sign up</p>
-                    </Button>
+                    </button>
                 </div>
             </div>
             {renderModal()}
